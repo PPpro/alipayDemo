6144cb8fdf2eda9f0c5d567a8359b90c7982fb68
PP <chinese1236@qq.com>
2019-07-21 15:30:16 +0800

    update

--- game.js
+++ game.js
@@ -1,15 +1,7 @@
 require('./my-adapter');
 
-
-// access 接口问题访问本地文件，复现平台：IDE
-// require('./demos/fs_access');
-
 // downloadFile 之后，readFile 失败，复现平台：IDE，华为荣耀9 【已修复】
 // require('./demos/fs_readFile');
-
-// 没办法提供简单 demo：
-// touch 事件：屏幕边缘不能触发 touch 事件，复现平台：安卓机
-// timeout 回调缺失 【已修复】
 
 // performance_now 返回的时间错误 【已修复】
 // require('./demos/performance_now');
@@ -17,8 +9,14 @@ require('./my-adapter');
 // image onload 回调问题，复现平台：安卓机 【已修复】
 // require('./demos/loadImage');
 
-// webGlRenderingContext.canvas 引用错误，复现平台：安卓机
+// webGlRenderingContext.canvas 引用错误，复现平台：安卓机 【已修复】
 // require('./demos/canvas');
+
+// access 接口问题访问本地文件，复现平台：IDE
+// require('./demos/fs_access');
+
+// 陀螺仪没有立即开启，开启有一段时间延时, 在华为荣耀9 上没有执行回调
+// require('./demos/acceleration');
 
 // webgl stencil 遮罩无效，复现平台：华为荣耀9
 // require('./demos/gl_stencil');
@@ -26,8 +24,8 @@ require('./my-adapter');
 // 音频：使用拷贝后的路径，播放失败，复现平台：安卓机
 // require('./demos/audio');
 
-// 陀螺仪没有立即开启，开启有一段时间延时, 在华为荣耀9 上没有执行回调
-// require('./demos/acceleration');
-
 // keyboard 软键盘上没有出现输入框， 复现平台：小米5
-require('./demos/keyboard');
+// require('./demos/keyboard');
+
+// touch 事件：屏幕上边缘不能触发 touch 事件，复现平台：安卓机
+// 这里应该不需要 demo 了
